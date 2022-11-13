7b14a5bd05f6aebfe7f8517dfc3ea898ee949cb8
Taufique Azad <Taufique.Azad@mountainwarehouse.com>
2022-11-13 19:29:05 +0000

    Q3: Find the smallest letter

---
+++ 1.1 BinarySearchWithInterviewQuestions/Q3-Smallest Letter/Program.cs
@@ -0,0 +1,30 @@
+﻿//Find the Smallest letter greater than the target
+
+//Given a charcters array letters that is sorted in non-decreasing(Ascending) order and a character target, return the smallest character 
+//in the array that is larger than the target
+
+char[] arr = { 'c', 'f', 'j' };
+char target = 'j';
+
+Console.WriteLine(findingSmallestLetter(arr,target));
+
+static char findingSmallestLetter(char[] arr, char target)
+{
+    int start = 0;
+    int end = arr.Length - 1;
+
+    while(start <= end)
+    {
+        int mid = start + (end - start)/2;
+
+        if (target > arr[mid])
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        } 
+    }
+    return arr[start % arr.Length];
+}
