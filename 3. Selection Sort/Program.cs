c0ca2437ea418d3adac370522c03a3065aae1011
Taufique Azad <Taufique.Azad@mountainwarehouse.com>
2022-12-28 18:17:35 +0000

    Selection Sort

---
+++ 3. Selection Sort/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] arr = { 4, 5, 1, 2,3 };
+
+SelectionSort(arr);
+
+
+static void SelectionSort(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int last = arr.Length - i - 1;
+        int maxIndex  = MaxIndex(arr, 0, last);
+        int temp = arr[last];
+        arr[last] = arr[maxIndex];
+        arr[maxIndex] = temp;
+    }
+
+    Array.ForEach(arr, Console.WriteLine);
+}
+
+static int MaxIndex(int[] arr, int start, int end)
+{
+    int max = start;
+    for (int i = start; i <= end; i++)
+	{
+        if (arr[max] < arr[i])
+        {
+            max = i;
+		}
+	}
+    return max;
+}
