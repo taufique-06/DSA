66af8f9fd8ce7e6eaf64b3f16a1bf9b2be944744
Taufique Azad <Taufique.Azad@mountainwarehouse.com>
2022-12-28 19:41:00 +0000

    Insertion Sort

---
+++ 4. Insertion Sort/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] arr = {3,4,-8,0,45,1 };
+InsertionSort(arr);
+Array.ForEach(arr, Console.WriteLine);
+
+
+static void InsertionSort(int[] arr)
+{
+	for (int i = 0; i < arr.Length - 1; i++)
+	{
+		for (int j = i+1; j >0; j--)
+		{
+			if (arr[j] < arr[j - 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j - 1];
+				arr[j - 1] = temp;
+			}else
+			{
+                break;
+            }
+
+		}
+	}
+}
