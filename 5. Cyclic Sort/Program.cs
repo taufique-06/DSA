363862dd055a73f79959dcd35e9e88b53511db69
Taufique Azad <Taufique.Azad@mountainwarehouse.com>
2022-12-28 20:04:19 +0000

    Cyclic Sort

---
+++ 5. Cyclic Sort/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] arr = { 3, 5, 2, 1, 4 };
+CyclicSort(arr);
+Array.ForEach(arr, Console.WriteLine);
+
+static void CyclicSort(int[] arr)
+{
+    int i = 0;
+    while (i < arr.Length)
+    {
+        int correctIndex = arr[i] - 1;
+        if(arr[i] != arr[correctIndex])
+        {
+            int temp = arr[i];
+            arr[i] = arr[correctIndex];
+            arr[correctIndex] = temp;
+        }else
+        {
+            i++;
+        }
+    }
+}
