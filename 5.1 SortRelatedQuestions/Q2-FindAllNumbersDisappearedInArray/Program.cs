f69458ae7dc19e3e4c1c207c94d38527ad20c2ac
Taufique Azad <Taufique.Azad@mountainwarehouse.com>
2022-12-30 15:27:03 +0000

    Q3

---
+++ 5.1 SortRelatedQuestions/Q2-FindAllNumbersDisappearedInArray/Program.cs
@@ -0,0 +1,31 @@
+﻿//https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+int[] nums = { 4, 3, 2, 7, 8, 2, 3, 1 };
+
+FindMissingNumbers(nums);
+
+
+static List<int> FindMissingNumbers(int[] nums)
+{
+    int i = 0;
+    while (i < nums.Length)
+    {
+        int currentIndex = nums[i] -1 ;
+        if (nums[i] != nums[currentIndex])
+        {
+            int temp = nums[i];
+            nums[i] = nums[currentIndex];
+            nums[currentIndex] = temp;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    List<int> result = new List<int>();
+    for (int j = 0; j < nums.Length; j++)
+    {
+        if (nums[j] != j+1) { result.Add(j+1); }
+    }
+    result.ForEach(Console.WriteLine);
+    return result;
+}
---
+++ 5.1 SortRelatedQuestions/Q3-FindTheDuplicateNumber/Program.cs
@@ -0,0 +1,61 @@
+﻿//int[] nums = { 1, 3, 4, 2, 2 };
+//int[] nums = { 3, 1, 3, 4, 2 };
+int[] nums = { 1, 1 };
+Console.WriteLine(FindDuplicate(nums));
+//Array.ForEach(nums, Console.WriteLine);
+
+static int FindDuplicate(int[] nums)
+{
+    int i =0;
+    while(i < nums.Length)
+    {
+        int currentIndex = nums[i] - 1;
+        if (nums[i] != nums[currentIndex])
+        {
+            int temp = nums[i];
+            nums[i] = nums[currentIndex];
+            nums[currentIndex] = temp;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    for (int j = 0; j < nums.Length; j++)
+    {
+        if (nums[j] != j + 1)
+        {
+            return nums[j];
+        }
+    }
+    return 0 ;
+}
+
+//Second way to find the duplicate
+
+static int FindDuplicate2(int[] nums)
+{
+    int i = 0;
+    while (i < nums.Length)
+    {
+        if (nums[i] != i + 1) 
+        { 
+            int currentIndex = nums[i] - 1;
+            if (nums[i] != nums[currentIndex])
+            {
+                int temp = nums[i];
+                nums[i] = nums[currentIndex];
+                nums[currentIndex] = temp;
+            }
+            else
+            {
+                return nums[i];
+            }
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return -1;
+}
